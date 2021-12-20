bbca729cbc6be27ea3d8b4fa4761dc6d463aa646
root <root@localhost.localdomain>
2021-12-20 16:24:21 +0800

    .

--- web/admin/src/main/java/com/zookao/admin/config/RocketmqTest.java
+++ web/admin/src/main/java/com/zookao/admin/config/RocketmqTest.java
@@ -1,10 +1,10 @@
-package com.zookao.admin;
+package com.zookao.admin.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.Message;
