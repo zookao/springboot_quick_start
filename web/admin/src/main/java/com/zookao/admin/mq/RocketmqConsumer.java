e96f149d26708367095acc6f511f6e2de9ff72ce
root <root@localhost.localdomain>
2021-12-20 17:23:19 +0800

    .

--- web/admin/src/main/java/com/zookao/admin/mq/RocketmqConsumer.java
+++ web/admin/src/main/java/com/zookao/admin/mq/RocketmqConsumer.java
@@ -1,4 +1,4 @@
-package com.zookao.admin.config;
+package com.zookao.admin.mq;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
@@ -6,13 +6,14 @@ import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
 /**
+ * 注意：Consumer里抛出异常才会重试，所以使用者不要把Consumer里的整个代码try-catch
  * User: zookao
  * Date: 2021-12-20
  */
 @Slf4j
 @Component
 @RocketMQMessageListener(topic = "test_producer", consumerGroup = "admin")
-public class RocketmqListener implements RocketMQListener<String> {
+public class RocketmqConsumer implements RocketMQListener<String> {
 
     @Override
     public void onMessage(String message) {
--- web/admin/src/main/java/com/zookao/admin/mq/RocketmqProducer.java
+++ web/admin/src/main/java/com/zookao/admin/mq/RocketmqProducer.java
@@ -1,27 +1,35 @@
-package com.zookao.admin.config;
+package com.zookao.admin.mq;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 /**
  * User: zookao
  * Date: 2021-12-20
  */
-@SpringBootTest
-@Slf4j
-public class RocketmqTest {
+@Component
+@EnableScheduling
+public class RocketmqProducer {
 
     @Autowired
     private RocketMQTemplate rocketMQTemplate;
 
-    @Test
+    // 秒（0~59） 例如0/5表示每5秒
+    // 分（0~59）
+    // 时（0~23）
+    // 日（0~31）的某天，需计算
+    // 月（0~11）
+    // 周几（ 可填1-7 或 SUN/MON/TUE/WED/THU/FRI/SAT）
+    // @Scheduled(cron = "0/5 * * * * ?")
+    //或直接指定时间间隔，例如：5秒
+    @Scheduled(fixedRate = 5000)
     public void syncSend() {
         for (int i = 0; i < 7; i++) {
             // 同步消息
@@ -31,7 +39,6 @@ public class RocketmqTest {
         }
     }
 
-    @Test
     public void asyncSend() {
         for (int i = 0; i < 7; i++) {
             // 异步消息
@@ -50,7 +57,6 @@ public class RocketmqTest {
         }
     }
 
-    @Test
     public void sendOneWay() {
         for (int i = 0; i < 7; i++) {
             // 单向发送消息
@@ -60,7 +66,6 @@ public class RocketmqTest {
         }
     }
 
-    @Test
     public void syncSendOrder() {
         // 发送有序消息
         String[] tags = new String[]{"TagA", "TagC", "TagD"};
@@ -76,7 +81,6 @@ public class RocketmqTest {
      * 发送延时消息
      * 在start版本中 延时消息一共分为18个等级分别为：1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h<br/>
      */
-    @Test
     public void sendDelayMsg() {
         Message<String> message = new GenericMessage<String>("测试延时消息");
         rocketMQTemplate.syncSend("test_producer", message, 3000, 5);
